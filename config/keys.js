4b408cfbb1eedb5a59ff1987cdf318b902024cf8
Black Sky <sunnyjain15699@gmail.com>
2019-09-06 12:43:49 +0530

    this is done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:'mongodb://Sunny Jain:Sunny123Rishi123@ds061198.mlab.com:61198/storybook-dev',
-    client_id:"187628863161-eccsctotf4pts4bv6o4lerkn6jjmlm4c.apps.googleusercontent.com",
-    client_secret:"ZccYyqxdLBSP_qlQEZv7qQsP"
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:   process.env.MONGO_URI,
+    client_id:  process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET
+}
